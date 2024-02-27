8a1f97ecbd772c37665adf43eee914c2d873fefe
Ben Hilger <bhilger78@gmail.com>
2024-02-26 20:40:43 -0500

    feat: cleanup naming and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,13 +18,13 @@ export default function Home() {
   const [icons, setIcons] = useState<GameTile[]>(Array(squareDimensions ** 2).fill(""));
   const [selected, setSelected] = useState<number[]>([]);
 
-  const numberCorrect = useMemo(() => {
+  const numberOfPairsCorrect = useMemo(() => {
     return icons.reduce((prev, curr) => curr.correct ? prev + 1 : prev, 0) / 2
   }, [icons])
 
   const allCorrect = useMemo(() => {
-    return numberCorrect === getSize() / 2;
-  }, [numberCorrect])
+    return numberOfPairsCorrect === getSize() / 2;
+  }, [numberOfPairsCorrect])
 
   const availableIcons: string[] = [
     "fa-code",
@@ -41,7 +41,6 @@ export default function Home() {
   useEffect(function() {
     setup()
   }, [])
-
 
   useEffect(function() {
     if (selected.length === 2) {
@@ -71,7 +70,7 @@ export default function Home() {
   }
 
   function generateIconsToUse() {
-    const iconsNeeded = squareDimensions ** 2 / 2;
+    const iconsNeeded = getSize() / 2;
     const icons = availableIcons.slice(0, iconsNeeded);
 
     let iconStates: GameTile[] = [];
@@ -113,7 +112,7 @@ export default function Home() {
     return "";
   }
 
-  function generateDeck() {
+  function generateGameBoard() {
     return Array.from(Array(squareDimensions), (e, i) => {
       return <div className="flex flex-row" key={i}>
         {
@@ -133,11 +132,13 @@ export default function Home() {
   }
 
   return (
-    <div key="1" className="flex flex-col w-screen h-screen justify-center items-center">
-      <p>{numberCorrect} / {getSize() / 2} correct</p>
-      {generateDeck()}
+    <div className="flex flex-col w-screen h-screen justify-center items-center">
+      <p>{numberOfPairsCorrect} / {getSize() / 2} correct</p>
+      {generateGameBoard()}
       {renderCompleteText()}
-      <button type="button" onClick={setup} className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+      <button type="button"
+        onClick={setup}
+        className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
         Reset
       </button>
     </div>
