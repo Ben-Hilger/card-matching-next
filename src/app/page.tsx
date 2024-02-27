a9ed7b4ca26870895e35227a4a72f6f951109f40
Ben Hilger <bhilger78@gmail.com>
2024-02-26 20:35:09 -0500

    feat: add text once the user correctly compltes the game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,10 +125,18 @@ export default function Home() {
     })
   };
 
+  function renderCompleteText() {
+    if (allCorrect) {
+      return <p>Congratulations! You've correctly completed the puzzle</p>
+    }
+    return <></>
+  }
+
   return (
     <div key="1" className="flex flex-col w-screen h-screen justify-center items-center">
       <p>{numberCorrect} / {getSize() / 2} correct</p>
       {generateDeck()}
+      {renderCompleteText()}
       <button type="button" onClick={setup} className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
         Reset
       </button>
